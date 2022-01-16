979a7216e055d68662d79b33debb014832815d58
Josvany García <jgarcia@cgclatam.com>
2022-01-16 10:52:17 -0600

    componente row details

---
+++ BethanysPieShopHRM.App/Components/EmployeeRowBase.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public class EmployeeRowBase : ComponentBase
+    {
+        [Parameter]
+        public Employee Employee { get; set; }
+    }
+}
